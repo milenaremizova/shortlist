04e65ca2fc615630d66f090d2a20929ea78c026d
Milena Remizova <remizovamilenaaa@yandex.ru>
2026-09-12 15:51:18 +0300

    test(domain): cover odd-sized pool

--- src/domain/strategies/MergeSortStrategy.test.ts
+++ src/domain/strategies/MergeSortStrategy.test.ts
@@ -40,27 +40,57 @@ describe("MergeqSortStrategy", () => {
     expect(pairAC).toEqual({ left: a, right: c });
   });
 
-  it('сортировка 4ых фильмов до конца', () => {
-  // Arrange
-  const a = makeMovie('a');
-  const b = makeMovie('b');
-  const c = makeMovie('c');
-  const d = makeMovie('d');
-  const strategy = new MergeSortStrategy([a, b, c, d], identity);
+  it("сортировка 4ых фильмов до конца", () => {
+    // Arrange
+    const a = makeMovie("a");
+    const b = makeMovie("b");
+    const c = makeMovie("c");
+    const d = makeMovie("d");
+    const strategy = new MergeSortStrategy([a, b, c, d], identity);
 
-  // Act
-  let pair = strategy.nextPair();
-  while (pair !== null) {
-    strategy.submit({
-      leftId: pair.left.id,
-      rightId: pair.right.id,
-      winnerId: pair.left.id,
+    // Act
+    let pair = strategy.nextPair();
+    while (pair !== null) {
+      strategy.submit({
+        leftId: pair.left.id,
+        rightId: pair.right.id,
+        winnerId: pair.left.id,
+      });
+      pair = strategy.nextPair();
+    }
+
+    // Assert
+    expect(strategy.getRanking()).toEqual({
+      places: [a, b, c, d],
+      isFinal: true,
     });
-    pair = strategy.nextPair();
-  }
+    expect(strategy.isComplete()).toBe(true);
+  });
+  it("верно сортирует нечетное число фильмов", () => {
+    // Arrange
+    const a = makeMovie("a");
+    const b = makeMovie("b");
+    const c = makeMovie("c");
+    const d = makeMovie("d");
+    const e = makeMovie("e");
+    const strategy = new MergeSortStrategy([a, b, c, d, e], identity);
 
-  // Assert
-  expect(strategy.getRanking()).toEqual({ places: [a, b, c, d], isFinal: true });
-  expect(strategy.isComplete()).toBe(true);
-});
+    // Act
+    let pair = strategy.nextPair();
+    while (pair !== null) {
+      strategy.submit({
+        leftId: pair.left.id,
+        rightId: pair.right.id,
+        winnerId: pair.left.id,
+      });
+      pair = strategy.nextPair();
+    }
+
+    // Assert
+    expect(strategy.getRanking()).toEqual({
+      places: [a, b, c, d, e],
+      isFinal: true,
+    });
+    expect(strategy.isComplete()).toBe(true);
+  });
 });
